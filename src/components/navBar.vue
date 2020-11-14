41bc28488cb4daaf4994e2d7f5eedc5aa073d887
John Bogart <john.bogart@bbc.co.uk>
2020-11-14 22:27:51 +0000

    Update navBar and add swansdom page

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -2,6 +2,7 @@
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-start">
        <nuxt-link to='/' class="navbar-item">Home</nuxt-link>
+       <nuxt-link to='/swansdom' class="navbar-item">Swansdom</nuxt-link>
     </div>
   </nav>
 </template>
---
+++ src/pages/swansdom.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Swansdom</h1>
+      </div>
+    </section>
+
+  </div>
+</template>

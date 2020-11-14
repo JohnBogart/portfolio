33032654c800a33e4d368e4593fe482ba33e3c72
John Bogart <john.bogart@bbc.co.uk>
2020-11-14 22:46:59 +0000

    Set up quotes component for swansdom page

---
+++ src/components/quotes.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button @click='newQuote'>Get New Quote</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Quote',
+  data: function () {
+    return {
+      quote: ''
+    }
+  },
+  methods: {
+    newQuote: function () {
+      alert('test')
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/pages/swansdom.vue
+++ src/pages/swansdom.vue
@@ -3,8 +3,19 @@
     <section class="section">
       <div class="container">
         <h1 class="title">Swansdom</h1>
+        <Quote />
       </div>
     </section>
-
   </div>
 </template>
+
+<script>
+import Quote from '../components/quotes.vue'
+
+export default {
+  name: 'Swansdom',
+  components: {
+    Quote
+  }
+}
+</script>

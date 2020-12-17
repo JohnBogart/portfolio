9a9b0a2d6ae18beec815c4c2f167367a1b9bab77
John Bogart <john.bogart@bbc.co.uk>
2020-12-17 15:38:23 +0000

    Basic fetch for character

--- src/pages/schwifty/character/_id.vue
+++ src/pages/schwifty/character/_id.vue
@@ -2,8 +2,24 @@
   <div>
     <section class="section">
       <div class="container">
-        <h1 class="title">Character - Name of Character</h1>
+        <h1 class="title">{{ character.name }}</h1>
       </div>
     </section>
   </div>
 </template>
+
+<script>
+import fetch from 'node-fetch'
+
+export default {
+  data() {
+    return {
+      character: {}
+    }
+  },
+  async fetch() {
+    const id = this.$route.params.id
+    this.character = await fetch(`https://rickandmortyapi.com/api/character/${id}`).then(res => res.json())
+  }
+}
+</script>

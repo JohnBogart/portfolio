2f630d344bf5955d29d7f7c4d1f362da3c731f00
John Bogart <john.bogart@bbc.co.uk>
2020-12-17 10:58:56 +0000

    schwifty home page

---
+++ src/pages/schwifty/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Schwifty</h1>
+        <h2 class="subtitle">Everything you need to know about Rick and Morty! Show me what you got!</h2>
+      </div>
+    </section>
+  </div>
+</template>

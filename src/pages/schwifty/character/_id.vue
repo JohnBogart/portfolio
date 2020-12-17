55f2fa0d72d1fa1dc502c33fbcbce5fe7c0a3a15
John Bogart <john.bogart@bbc.co.uk>
2020-12-17 15:34:01 +0000

    Add basic dynamic page

---
+++ src/pages/schwifty/character/_id.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Character - Name of Character</h1>
+      </div>
+    </section>
+  </div>
+</template>

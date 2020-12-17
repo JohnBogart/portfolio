69e85afd8f826134a417acea1ca17edfc57d83d4
John Bogart <john.bogart@bbc.co.uk>
2020-12-17 15:42:50 +0000

    Fix NavBar

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -2,19 +2,24 @@
     <b-navbar type="is-black">
         <template slot="brand">
           <b-navbar-item tag="router-link" :to="{ path: '/' }">
-              John Bogart
+            John Bogart
           </b-navbar-item>
         </template>
         <template slot="start" type="is-black">
           <b-navbar-item href="/">
-              Home
+            Home
           </b-navbar-item>
           <b-navbar-item href="/swansdom">
-              Swansdom
+            Swansdom
           </b-navbar-item>
+          <b-navbar-dropdown label="Schwifty">
             <b-navbar-item href="/schwifty">
-              Schwifty
-          </b-navbar-item>
+              Home
+            </b-navbar-item>
+            <b-navbar-item href="/schwifty/character">
+              Characters
+            </b-navbar-item>
+          </b-navbar-dropdown>
         </template>
     </b-navbar>
 </template>

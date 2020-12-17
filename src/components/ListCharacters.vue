8c5061d7c07a6d2bb4d3ee36fee8a1716f67e90f
John Bogart <john.bogart@bbc.co.uk>
2020-12-17 15:28:02 +0000

    List characters (just the first five for now)

---
+++ src/components/ListCharacters.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul>
+      <li v-for='character in characters' :key='character.id'>
+        {{ character.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import fetch from 'node-fetch'
+
+export default {
+  data() {
+    return {
+      characters: []
+    }
+  },
+  async fetch() {
+    const { results } = await fetch('https://rickandmortyapi.com/api/character').then(res => res.json())
+    this.characters = results.slice(0, 5)
+  }
+}
+</script>
---
+++ src/pages/schwifty/character/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Rick and Morty Characters</h1>
+        <list-characters></list-characters>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import ListCharacters from '../../../components/ListCharacters.vue'
+
+export default {
+  components: {
+    ListCharacters
+  }
+}
+</script>

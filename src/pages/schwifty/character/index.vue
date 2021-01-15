6fbe5afa8556d946f5014ae070ebe225d35c0f58
John Bogart <john.bogart@bbc.co.uk>
2021-01-15 14:57:12 +0000

    Tweak

--- src/pages/schwifty/character/index.vue
+++ src/pages/schwifty/character/index.vue
@@ -5,7 +5,6 @@
         <h1 class="title">Rick and Morty Characters</h1>
         <p>Total Number of Characters: {{count}}</p>
         <list-characters :page="page"></list-characters>
-        <!-- pagination here -->
         <button v-for='page in pages' :key='page' @click="goToPage(page)">{{page}}</button>
       </div>
     </section>
@@ -24,15 +23,13 @@ export default {
     return {
       count: 0,
       page: 0,
-      totalPages: 0,
       pages: []
     }
   },
   async fetch() {
     const { info } = await fetch('https://rickandmortyapi.com/api/character').then(res => res.json())
     this.count = info.count
-    this.totalPages = info.pages
-    this.pages = Array.from(Array(this.totalPages)).map((e,i)=>i+1)
+    this.pages = Array.from(Array(info.pages)).map((e,i)=>i+1)
   },
   methods: {
     goToPage: function (page) {
